ababdd07c61a80ab7edbb0648eee888914ad5294
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-06-16 00:48:04 +0300

    2

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-date=$(date -v-1m +%d-%m-%Y)
+date=$(date -v-20d +%d-%m-%Y)
 echo $date
 git ls-files | while read path
 do
   echo checking $path $(git log -1 --format=" %ad" -- $path)
-if [[ $path == docs* ]]; then
-        continue
-    fi
 if [ "$(git log --since \"$date\" -- $path)" == "" ]
   then
     if [[ $path == "cleanup.sh" || $path == "static.json" ]]; then
