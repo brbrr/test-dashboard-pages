507bbfd6751efd29459fa5e71094f1d0492ea5a3
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-09-01 12:02:42 +0300

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-date=$(date -v-20d +%Y-%m-%d)
+if [ "$(uname)" == "Darwin" ]; then
+    date=$(date -v-20d +%Y-%m-%d)       
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    date --date="20 days ago" +%Y-%m-%d
+fi
+
 echo $date
 # git ls-files -- . ':!:docs*' | while read file
 git ls-tree --name-only master | while read file
